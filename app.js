6d0c77fe6af6d8abc5587cc102f0fd6278d94a1b
xeni85 <shkelzen.dunisha@umsh.edu.al>
2023-02-21 04:34:02 -0500

    added some functionality

--- app.js
+++ app.js
@@ -17,7 +17,8 @@ const aliens = {
             return firepower;
         },
         accuracy: () => {
-            let accuracy = (Math.random() * (0.8 - 0.6) + 0.6).toFixed(2);
+            let accuracy = Math.random() * (0.8 - 0.6) + 0.6;
+            return accuracy.toFixed(2);
         },
     },
     second: {
@@ -30,7 +31,8 @@ const aliens = {
             return firepower;
         },
         accuracy: () => {
-            let accuracy = (Math.random() * (0.8 - 0.6) + 0.6).toFixed(2);
+            let accuracy = Math.random() * (0.8 - 0.6) + 0.6;
+            return accuracy.toFixed(2);
         },
     },
     third: {
@@ -43,7 +45,8 @@ const aliens = {
             return firepower;
         },
         accuracy: () => {
-            let accuracy = (Math.random() * (0.8 - 0.6) + 0.6).toFixed(2);
+            let accuracy = Math.random() * (0.8 - 0.6) + 0.6;
+            return accuracy.toFixed(2);
         },
     },
     fourth: {
@@ -56,7 +59,8 @@ const aliens = {
             return firepower;
         },
         accuracy: () => {
-            let accuracy = (Math.random() * (0.8 - 0.6) + 0.6).toFixed(2);
+            let accuracy = Math.random() * (0.8 - 0.6) + 0.6;
+            return accuracy.toFixed(2);
         },
     },
     fifth: {
@@ -69,7 +73,8 @@ const aliens = {
             return firepower;
         },
         accuracy: () => {
-            let accuracy = (Math.random() * (0.8 - 0.6) + 0.6).toFixed(2);
+            let accuracy = Math.random() * (0.8 - 0.6) + 0.6;
+            return accuracy.toFixed(2);
         },
     },
     sixth: {
@@ -82,8 +87,8 @@ const aliens = {
             return firepower;
         },
         accuracy: () => {
-            let accuracy = (Math.random() * (0.8 - 0.6) + 0.6).toFixed(2);
-            return accuracy;
+            let accuracy = Math.random() * (0.8 - 0.6) + 0.6;
+            return accuracy.toFixed(2);
         },
     },
 };
@@ -95,17 +100,101 @@ let isOn = false;
 
 //grab nodes
 let footer = document.querySelector("footer");
+let playerImg = document.querySelector("#player");
+let alienImg = document.querySelector("#alien");
+let playerLaser = document.querySelector(".player-laser");
+let alienLaser = document.querySelector(".alien-laser");
+console.log(alienImg + " This is alien Img")
+console.log(playerImg + " This is player Img")
 let playerAttackDiv = document.querySelector(".player-attack");
 let playerAttackBtn = document.querySelector("#player-attack-btn");
 let alienContainer = document.querySelector(".alien-container");
 let playerContainer = document.querySelector(".player-container");
 let hFour = document.querySelector("h4");
 let otherAliens = document.querySelector(".other-aliens");
+let alienP1 = document.getElementById('alienP1');
+let alienP2 = document.getElementById('alienP2');
+let alienP3 = document.getElementById('alienP3');
+let playerP1 = document.getElementById('playerP1');
+let playerP2 = document.getElementById('playerP2');
+let playerP3 = document.getElementById('playerP3');
 //counter to change button
 let counter = 0;
 
 //game object
 const game = {
+
+    insertPlayerData: (player1) => {
+        playerP1.textContent = `player hull: ${player1.hull}`
+        playerP2.textContent = `Player accuracy: ${player1.accuracy}`
+        playerP3.textContent = `Player firepower: ${player1.firepower}`
+    },
+    insertAlienData: (player2, hull) => {
+        alienP1.textContent = `Alien hull: ${hull}`
+        alienP2.textContent = `Alien accuracy: ${player2.accuracy()}`
+        alienP3.textContent = `Alien firepower: ${player2.firepower()}`
+    },
+    startHasBeenPressed: () => {
+        playerAttackBtn.remove();
+        hFour.remove();
+        playerAttackDiv.classList.remove("player-attack");
+        playerAttackDiv.classList.add("player-attack-inGame");
+    },
+    playerSuccessfulAttack: (player1, t) => {
+        setTimeout(() => {
+            alienLaser.classList.remove("alien-laser-miss");
+            alienLaser.classList.remove("alien-laser-hit");
+            alienImg.classList.remove("alien-inGame");
+            playerLaser.classList.remove("player-laser-miss");
+            playerLaser.classList.add("player-laser-hit");
+            playerImg.classList.add("player-inGame");
+
+
+            playerAttackDiv.textContent =
+                `Great shot, you caused the Alien ${player1.firepower} damage`;
+        }, t);
+    },
+    playerFailedAttack: (t) => {
+        setTimeout(() => {
+            alienLaser.classList.remove("alien-laser-hit");
+            alienLaser.classList.remove("alien-laser-miss");
+            playerLaser.classList.remove("player-laser-hit");
+            alienImg.classList.remove("alien-inGame");
+            playerLaser.classList.add("player-laser-miss");
+            playerImg.classList.add("player-inGame");
+
+
+            playerAttackDiv.textContent = "You missed, shoot again!";
+        }, t);
+    },
+    alienSuccessfulAttack: (player1,player2, t) => {
+        setTimeout(() => {
+            alienLaser.classList.remove("alien-laser-miss");
+            playerImg.classList.remove("player-inGame");
+            playerLaser.classList.remove("player-laser-hit");
+            playerLaser.classList.remove("player-laser-miss");
+            alienLaser.classList.add("alien-laser-hit");
+            alienImg.classList.add("alien-inGame");
+
+
+            playerP1.textContent = `player hull: ${player1.hull}`
+            playerAttackDiv.textContent =
+                `On No, you were hit! The Alien caused you ${player2.firepower()} damage`;
+        }, t);
+    },
+    alienFailedAttack: (t) => {
+        setTimeout(() => {
+            playerImg.classList.remove("player-inGame");
+            playerLaser.classList.remove("player-laser-hit");
+            playerLaser.classList.remove("player-laser-miss");
+            alienLaser.classList.remove("alien-laser-hit");
+            alienImg.classList.add("alien-inGame");
+            alienLaser.classList.add("alien-laser-miss");
+
+
+            playerAttackDiv.textContent = "Alien missed, you got lucky!";
+        }, t);
+    },
     beginGame: () => {
         if (counter === 0) {
             console.log("beginGame was clicked");
@@ -119,56 +208,58 @@ const game = {
     },
     attack: (player1, player2) => {
         if (counter > 1) {
+            game.startHasBeenPressed();
             let alienHull = player2.hull();
-            playerAttackBtn.remove();
-            hFour.remove();
-            playerAttackDiv.classList.remove("player-attack");
-            playerAttackDiv.classList.add("player-attack-inGame");
+            let timer = 3000;  
+            playerAttackDiv.textContent = "Press attack to begin!";
             for (let i = 0; i < Infinity; i++) {
-                if (isOn === false && alienHull > 0) {
-                    playerAttackDiv.textContent = "It's your turn to play";
-                    alienContainer.textContent = `Alien hull: ${alienHull}
-                    Alien accuracy: ${player2.accuracy()}
-                    Alien firepower: ${player2.firepower()}`
-
-                    console.log();
-                    console.log("AlienHull ---" + alienHull);
-
+                if (isOn === false && alienHull > 0 && player1.hull > 0) {
+                    game.insertPlayerData(player1);
+                    game.insertAlienData(player2, alienHull);
                     if (player1.accuracy > player2.accuracy()) {
+                        game.playerSuccessfulAttack(player1, timer.toString())
                         alienHull -= player1.firepower;
+                        timer+=3000;
+                        if(alienHull <= 0 ) {
+                            console.log("I-m in here");
+                            setTimeout(() => {
+                                playerAttackDiv.textContent = "YAY, Alien was defeated!";
+                                playerAttackDiv.append(playerAttackBtn);
+                            }, timer.toString());
+                            timer+=3000;
+                        }
+                    } else {
+                        game.playerFailedAttack(timer.toString())
+                        console.log(timer + " player attack failed");
+                        timer+=3000;
                     }
                     isOn = true;
-                } else if (isOn === true && alienHull > 0) {
-                    console.log("Player 2 turn");
-                    console.log(
-                        "Part 2 " + player1.accuracy + "------" + player2.accuracy()
-                    );
-
-                    console.log("AlienHull ---" + alienHull);
+                } else if (isOn === true && alienHull > 0 && player1.hull > 0) {
                     if (player2.accuracy() > player1.accuracy) {
                         player1.hull -= player2.firepower();
-                        console.log("line 116 " + player1.hull);
+                        game.alienSuccessfulAttack(player1, player2, timer.toString())
+                        console.log(timer + " alien attack successful")
+                        timer+=3000;
+                    } else {
+                        game.alienFailedAttack(timer.toString());
+                        console.log(timer + " alien attack failed")
+                        timer+=3000;
                     }
                     isOn = false;
                 } else {
-                    console.log(
-                        "Part 3 " + player1.accuracy + "------" + player2.accuracy()
-                    );
                     console.log("Player defeated");
                     console.log("AlienHull ---" + alienHull);
-
                     if (alienArray.length - 1 > 0) {
                         alienArray.shift();
                     } else {
-                        console.log("Game over, you won!!");
+                        console.log("Game over, you won!!")
                     }
-
-                    otherAliens.removeChild(otherAliens.getElementsByTagName("img")[0]);
-
-                    playerAttackDiv.append(playerAttackBtn);
+                    timer = 3000;
+                    otherAliens.removeChild(otherAliens.getElementsByTagName("img")[0])
                     break;
                 }
             }
+            playerAttackDiv.append(playerAttackBtn);
             console.log(alienArray);
         }
     },
